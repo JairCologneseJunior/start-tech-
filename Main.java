95e47e55eb5716e9e088b113be594a877afe3e20
JairCologneseJunior <102156480+JairCologneseJunior@users.noreply.github.com>
2022-11-10 08:36:14 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,71 @@
+public class Main {
+    public static void main(String[]args){
+//
+//       System.out.println(idade);
+//        int receba = 12;
+//        System.out.printf("o numero é %d", receba );
+
+
+//      int ano = 2003;
+//       System.out.printf("voce nasceu em %d", ano);
+//        int idade = 2022 - ano;
+//       System.out.printf(" a idade è %d em 2022 ",idade );
+
+//at 2
+
+//        int fileiras = 40;
+//        int cadeiras = 8;
+//        int cap = 40 *8;
+//        System.out.printf("a capacide de pessoas na sala é de %d \n", cap);
+//
+//        int especial = (cap * 10) / 100;
+//
+//        System.out.printf("e os 10 porcento é de %d cadeiras", especial );
+        //at3
+
+//        double km = 42;
+//        double milhas =0.62137 * km;
+//        System.out.printf("42km para milhas é %2f" , milhas);
+
+//at4
+//        double real = 30.00;
+//        double um =  5.3;
+//        double dois =  5.16;
+//        double tres =  5.87;
+//        double quatro =  0.03;
+//        double cinco =  0.04;
+//
+//        double dolar = real * um ;
+//        System.out.printf("converção em dolar %f \n", dolar);
+//        double euro = real * dois ;
+//        System.out.printf("converção em euro %f \n", euro);
+//        double libra = real * tres ;
+//        System.out.printf("converção em libras %f \n", libra);
+//        double peso = real * quatro ;
+//        System.out.printf("converção em pesos %f \n", peso);
+//        double iene = real * cinco ;
+//        System.out.printf("converção em iene %f \n", iene);
+//
+
+//at5
+
+//        double celsios = 30;
+//        double fere = (30 * 9/5) + 32;
+//        System.out.printf("%.0f em fahrenheit é %.0f", celsios, fere);
+
+        //at 7
+//
+//        double l = 13;
+//        double c = 20;
+//        double p = 1.9;
+//
+//        double cubicos = l * c* p;
+//
+//        double litros = cubicos * 1000;
+//        System.out.printf("os metros cubicos sao %f e em litros %f",  cubicos, litros);
+
+
+    }
+}
+
+
